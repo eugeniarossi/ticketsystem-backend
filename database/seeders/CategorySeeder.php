54f0144fc59b235bc64b5ec031a5779e5ecf76f1
eugeniarossi <eugenia.rossi@outlook.com>
2023-10-27 15:09:41 +0200

    feat: category seeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 10; $i++) {
+
+            $newCategory = new Category();
+
+            $newCategory->name = $faker->sentence(1);
+
+            $newCategory->save();
+        }
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            TicketSeeder::class
+            TicketSeeder::class,
+            CategorySeeder::class
         ]);
     }
 }
